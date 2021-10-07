8bca190c9efc60e08b5d1ba54a9cb8be45f156c3
Leo Aceituno <leonardo.aceituno@gmail.com>
2021-10-07 14:18:12 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container w-50">
+    <h2>Lista de Tareas</h2>
+    <div class="d-flex justify-content-between">
+      <input 
+        type="text" 
+        class="form-control w-75" 
+        v-model='tarea'
+        @keyup.enter='agregarTarea' 
+      />
+      <button
+        type="button"
+        class="btn btn-primary"
+        @click="agregarTarea"
+      >
+        Agregar
+      </button>
+
+    </div>    
+    <desplegar 
+      :listaTareas = listaTareas 
+      @eliminarTarea="eliminarTareaPadre"
+    />
+  
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // import HelloWorld from './components/HelloWorld.vue'
+  import desplegar from './components/desplegar.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+
+    data(){
+      return {
+        tarea:"",
+        listaTareas: [],
+      }
+    },
+
+    components: {
+      // HelloWorld
+      desplegar
+    },
+    
+    methods:{
+      agregarTarea(){
+        if(this.tarea == ""){
+          alert('* no ha ingresado ninguna tarea')
+        }else{
+        this.listaTareas.push(this.tarea)
+        this.tarea=""
+        }
+      },
+      eliminarTareaPadre(childValue){
+        this.listaTareas.splice(childValue,1)
+      }
+
+    }
+  };
 </script>
 
 <style>
---
+++ src/components/desplegar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-for="(item, index) in listaTareas" :key="index">
+    <div class="alert alert-secondary mt-3 text-start" role="alert">
+       <div class="d-flex justify-content-between align-items-center">
+            <div >{{ index + 1 }} : {{ item }}</div>
+            <button 
+                type="button" 
+                class="btn btn-sm btn-danger" 
+                @click="eliminar(index)"
+            >
+                Eliminar
+            </button>
+       </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "desplegar",
+  props: {
+    listaTareas: Array,
+  },
+  methods: {
+      eliminar(indice) {
+          this.$emit("eliminarTarea",indice)
+      }
+  }
+  
+};
+</script>
+
+<style>
+</style>
